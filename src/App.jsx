79500b061e75f2dd33d9ffbd50a225e9d13ebe88
Humayra Binte Jamal <humayra1111@gmail.com>
2025-09-14 22:36:43 +0600

    added works section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import According from "./components/Accodring/According";
 import Banner from "./components/banner-components/Banner";
 import Nav from "./components/Nav";
 import Footer from "./components/shared/Footer";
-
+import HowItWorks from "./components/HowItWorks/HowItWorks";
 function App() {
   return (
     <>
@@ -12,6 +12,7 @@ function App() {
         <Banner />
       </nav>
       <div>
+        <HowItWorks/>
         <According></According>
       </div>
       <Footer />
